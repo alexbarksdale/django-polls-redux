408aca0e0e6b570d1c130f6075ed8c5381434cf5
alexbarksdale <ale.barksdale@gmail.com>
2020-04-01 02:27:08 -0500

    Trying to cooperate with gradescope

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 
 def index(req):
-    return HttpResponse("Hello world")
+    return HttpResponse("Hello, world. You're at the polls index.")
 
 # Create your views here.
